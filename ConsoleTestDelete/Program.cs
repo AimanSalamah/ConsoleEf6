a21f395fbbc8fed0005d0ab4e6b39ee1ab58657b
Aiman Salamah <aiman@gsoft.sa>
2022-02-07 17:40:57 +0300

    Try to update one column only without loading full entity from database

--- ConsoleTestDelete/Program.cs
+++ ConsoleTestDelete/Program.cs
@@ -1,69 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 
-var db = new Db(new DbContextOptions<Db>
-{
-});
 bool loop = true;
 while(loop)
 {
-    Console.WriteLine("create db\ndelete db\nfill db\ntest join\ntest delete\nexit");
-    switch(Console.ReadLine())
+    using (var db = new Db(new DbContextOptions<Db>()))
     {
-        case "create db":
-            db.Database.EnsureCreated();
-            break;
-        case "delete db":
-            db.Database.EnsureDeleted();
-            break;
-        case "fill db":
-            for (int i = 0; i < 50; i++)
-            {
-                db.Customers.Add(new Customers { Name = $"Customer {i}", UnitId = new Random().Next(20) });
-            }
-            for (int i = 0; i < 30; i++)
-            {
-                db.Units.Add(new Units { Name = $"Unit {i}" });
-            }
-            Console.WriteLine(db.SaveChanges());           
-            break;
-        case "test join":
-            var units = new List<Units>
-            {
-                new(){ Id = 0, Name = "test unut 0" },
-                new(){ Id = 1, Name = "test unut 1" },
-                new(){ Id = 2, Name = "test unut 2" },
-                new(){ Id = 3, Name = "test unut 3" },
-                new(){ Id = 4, Name = "test unut 4" },
-            };
-            var data = (from c in db.Customers.ToList()
-                        join u in units on c.UnitId equals u.Id
-                        select new Customers { Id = c.Id, Name = c.Name, UnitId = c.UnitId, UnitName = u.Name}).ToList();
-            break;
-        case "show data":
-            foreach (var custoemr in db.Customers)
-            {
-                Console.WriteLine($"id:{custoemr.Id}\tname:{custoemr.Name}\tunitid:{custoemr.UnitId}");
-            }
-            foreach (var unit in db.Units)
-            {
-                Console.WriteLine($"id:{unit.Id}\tname:{unit.Name}");
-            }
-            break;
-        case "test delete":
-            var customers = new List<Customers>
-            {
-                new Customers{ Id = 9},
-                new Customers{ Id = 10},
-                new Customers{ Id = 11},
-            };
-            db.Customers.RemoveRange(customers);
-            Console.WriteLine(db.SaveChanges());           
-            break;
-        case "exit":
-            loop = false;
-            break;
+        Console.WriteLine("create db\ndelete db\nfill db\nshow data\ntest join\ntest update\ntest delete\nexit");
+        switch(Console.ReadLine())
+        {
+            case "create db":
+                db.Database.EnsureCreated();
+                break;
+            case "delete db":
+                db.Database.EnsureDeleted();
+                break;
+            case "fill db":
+                for (int i = 0; i < 50; i++)
+                {
+                    db.Customers.Add(new Customers { Name = $"Customer {i}", UnitId = new Random().Next(20), Number = i });
+                }
+                for (int i = 0; i < 30; i++)
+                {
+                    db.Units.Add(new Units { Name = $"Unit {i}" });
+                }
+                Console.WriteLine(db.SaveChanges());           
+                break;
+            case "test join":
+                var units = new List<Units>
+                {
+                    new(){ Id = 0, Name = "test unut 0" },
+                    new(){ Id = 1, Name = "test unut 1" },
+                    new(){ Id = 2, Name = "test unut 2" },
+                    new(){ Id = 3, Name = "test unut 3" },
+                    new(){ Id = 4, Name = "test unut 4" },
+                };
+                var data = (from c in db.Customers.ToList()
+                            join u in units on c.UnitId equals u.Id
+                            select new Customers { Id = c.Id, Name = c.Name, UnitId = c.UnitId, UnitName = u.Name}).ToList();
+                break;
+            case "test update":
+                var UpdateModel = new Customers { Id = 9 };
+                Console.WriteLine($"Olde customer data: {UpdateModel}");
+                UpdateModel.Number = 0022002200;
+                db.Entry(UpdateModel).Property("Number").IsModified = true;
+                Console.WriteLine($"Save result: {db.SaveChanges()}");
+                break;
+            case "show data":
+                foreach (var custoemr in db.Customers)
+                {
+                    Console.WriteLine($"id:{custoemr.Id}\tname:{custoemr.Name}\tnumber:{custoemr.Number}\tunitid:{custoemr.UnitId}");
+                }
+                foreach (var unit in db.Units)
+                {
+                    Console.WriteLine($"id:{unit.Id}\tname:{unit.Name}");
+                }
+                break;
+            case "test delete":
+                var customers = new List<Customers>
+                {
+                    new Customers{ Id = 9},
+                    new Customers{ Id = 10},
+                    new Customers{ Id = 11},
+                };
+                db.Customers.RemoveRange(customers);
+                Console.WriteLine(db.SaveChanges());           
+                break;
+            case "exit":
+                loop = false;
+                break;
+        }
     }
 }
 Console.ReadLine();
@@ -89,6 +95,7 @@ public class Customers
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public int Number { get; set; }
     public int UnitId { get; set; }
     [NotMapped]
     public string UnitName { get; set; }
